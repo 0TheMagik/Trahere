4f3b8adb1bb2783a783fc69a3d9dc3ad6e6143d0
Izzan Nawa Syarif <izzan.nsyarif@gmail.com>
2025-11-03 13:20:41 +0700

    fixing warning

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtQuickControls2/QQuickStyle>
 
 int main(int argc, char *argv[])
 {
+    // Use a non-native Qt Quick Controls style so Control customization (e.g. background)
+    // is supported. Call setStyle before creating the application / loading QML.
+    QQuickStyle::setStyle("Basic");
+
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
